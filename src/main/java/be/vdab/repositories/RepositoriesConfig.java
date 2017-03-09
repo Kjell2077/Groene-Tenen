84cdb93bcd46e8b255bfa9ffd9efc3767dd5b5be
kjell.leenknegt <kjell.leenknegt@OntCur-0E05C437.vdabheverlee.intra>
2017-03-09 15:52:59 +0100

    toevoegen entities, valueobjects, repositories, exceptions en services

---
+++ src/main/java/be/vdab/repositories/RepositoriesConfig.java
@@ -0,0 +1,12 @@
+package be.vdab.repositories;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+// enkele imports
+
+@Configuration
+@ComponentScan
+public class RepositoriesConfig {
+
+}
